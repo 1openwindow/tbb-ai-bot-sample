6aced8b02f9130f3fcc53e93701baccf783b0f99
Zihong Chen <zihch@outlook.com>
2023-12-14 08:43:22 +0800

    feat: update function

--- functions/src/functions/caculator.ts
+++ functions/src/functions/caculator.ts
@@ -6,41 +6,15 @@ export async function caculator(request: HttpRequest, context: InvocationContext
     const firstNumber = request.query.get('firstNumber');
     const secondNumber = request.query.get('secondNumber');
 
-    if (!firstNumber || !secondNumber) {
-        return {
-            status: 400,
-            body: 'Please pass two numbers on the query string or in the request body'
-        };
-    }
+    let result = (Number(firstNumber) + Number(secondNumber)) * 4;
 
-    const first = Number(firstNumber);
-    const second = Number(secondNumber);
-
-    let result = (first + second) * 3;
-
-    // Convert to Roman Numerals
-    // let result = first + second;
-    // const decimalValues = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-    // const romanNumerals = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
-
-    // let roman = '';
-    // for (let i = 0; i < decimalValues.length; i++) {
-    //     while (result >= decimalValues[i]) {
-    //         result -= decimalValues[i];
-    //         roman += romanNumerals[i];
-    //     }
-    // }
-
-    // return {
-    //     body: `${roman}`
-    // };
     return {
         body: `${result}`
     };
 };
 
 app.http('caculator', {
-    methods: ['GET'],
+    methods: ['GET', 'POST'],
     authLevel: 'anonymous',
     handler: caculator
 });
